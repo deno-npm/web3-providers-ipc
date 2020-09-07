32b540d0c084ff16493c465d87ce10dbf4b058ec
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-06 19:45:30 -0500

    First working implementation

---
+++ deps.js
@@ -0,0 +1,3 @@
+export * as _ from "https://cdn.skypack.dev/-/underscore@v1.11.0-8iEGGigleKa9YmBQ2bJF/dist=es2020/underscore.js";
+export { default as oboe } from "https://cdn.skypack.dev/oboe@2.1.5";
+export * as utils from "https://denopkg.com/deno-npm/deno_npm/web3-utils/v1.2.11/mod.js";
